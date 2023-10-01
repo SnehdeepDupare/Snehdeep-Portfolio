daab94a46fb7be1528f7d4d3685429c1c5a0a113
Snehdeep <duparesnehdeep@gmail.com>
2023-10-01 23:50:03 +0530

    created type definitions

---
+++ typings.d.ts
@@ -0,0 +1,56 @@
+import { PortableTextBlock } from "sanity";
+
+interface SanityBody {
+  _createdAt: string;
+  _id: string;
+  _rev: string;
+  _updatedAt: string;
+}
+
+interface Image {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+}
+
+export interface Info extends SanityBody {
+  _type: "info";
+  aboutText: string;
+  title: string;
+  profilePic: Image;
+}
+
+export interface Technology extends SanityBody {
+  _type: "skill";
+  image: Image;
+  title: string;
+}
+
+export interface Skill extends SanityBody {
+  _type: "skill";
+  image: Image;
+  title: string;
+}
+
+export interface Screenshot extends SanityBody {
+  _type: "image";
+  asset: {
+    _ref: string;
+    _type: "reference";
+  };
+}
+
+export interface Project extends SanityBody {
+  _type: "project";
+  title: string;
+  slug: string;
+  poster: Image;
+  summary: string;
+  linkToBuild: string;
+  githubLink: string;
+  technologies: Technology[];
+  screenshots: Screenshot[];
+  description: PortableTextBlock[];
+}
