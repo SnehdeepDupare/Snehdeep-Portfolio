bd8484e7449618a0aa33f6a232b684671b43f1ef
Snehdeep <duparesnehdeep@gmail.com>
2023-10-01 23:51:47 +0530

    fetched data from sanity backend

--- app/(user)/about/page.tsx
+++ app/(user)/about/page.tsx
@@ -1,38 +1,45 @@
-"use client";
-import { motion } from "framer-motion";
-import { Metadata } from "next";
+// "use client";
+import urlFor from "@/sanity/lib/urlFor";
+import { getInfo } from "@/sanity/utils/getInfo";
+// import { motion } from "framer-motion";
 
-function About() {
+export const revalidate = 0;
+
+async function About() {
+  const info = await getInfo();
+
   return (
-    <motion.main
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 1.2 }}
+    <main
+      // initial={{ opacity: 0 }}
+      // whileInView={{ opacity: 1 }}
+      // transition={{ duration: 1.2 }}
       className="md:relative flex flex-col md:flex-row text-center mx-auto max-w-6xl px-10 justify-evenly items-center"
     >
       <h3 className="tracking-[20px] absolute top-24 md:top-0 uppercase text-2xl z-[-10] md:z-auto text-dimwhite">
         About
       </h3>
 
-      <motion.img
-        initial={{
-          x: -100,
-          opacity: 0,
-        }}
-        whileInView={{
-          x: 0,
-          opacity: 1,
-        }}
-        transition={{
-          duration: 1.2,
-        }}
-        src="/sd-profile.jpg"
+      <img
+        // initial={{
+        //   x: -100,
+        //   opacity: 0,
+        // }}
+        // whileInView={{
+        //   x: 0,
+        //   opacity: 1,
+        // }}
+        // transition={{
+        //   duration: 1.2,
+        // }}
+        src={urlFor(info?.profilePic).url()}
         alt="profile"
         className="rounded-full h-48 w-48 md:rounded-lg md:h-96 md:w-64 xl:h-[450px] xl:w-[350px] flex-shrink-0 object-cover mt-10"
       />
 
-      <motion.p className="px-0 md:px-10 mt-5 md:mt-0">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+      <p className="px-0 md:px-10 mt-5 md:mt-0">
+        {info?.aboutText}
+
+        {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
         doloremque commodi magnam, eaque accusamus ut assumenda. Veritatis unde
         fugit sequi eos corrupti saepe provident nobis nulla debitis? Eaque quae
         omnis itaque magni dolore corrupti aperiam accusamus facilis unde nemo
@@ -40,9 +47,9 @@ function About() {
         totam! Nisi quidem soluta cupiditate adipisci praesentium in omnis
         beatae dolorem officia tempora eos nulla, quam tempore nam repudiandae
         aut debitis voluptatem. Omnis sed ratione nesciunt optio suscipit
-        consectetur nam in necessitatibus provident ut
-      </motion.p>
-    </motion.main>
+        consectetur nam in necessitatibus provident ut */}
+      </p>
+    </main>
   );
 }
 
--- app/(user)/projects/page.tsx
+++ app/(user)/projects/page.tsx
@@ -1,51 +1,58 @@
-"use client";
+// "use client";
 import { TopRightArrow } from "@/components/Icons";
-import { motion } from "framer-motion";
+import urlFor from "@/sanity/lib/urlFor";
+import { getProjects } from "@/sanity/utils/getProjects";
+
+// import { motion } from "framer-motion";
 import Link from "next/link";
 
-function Projects() {
-  const pro = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+export const revalidate = 0;
+
+async function Projects() {
+  const projects = await getProjects();
 
   return (
     <>
-      <motion.main
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 1.2 }}
+      <main
+        // initial={{ opacity: 0 }}
+        // whileInView={{ opacity: 1 }}
+        // transition={{ duration: 1.2 }}
         className="max-w-6xl mx-auto flex flex-col items-center px-10"
       >
         <h3 className="tracking-[20px] uppercase text-2xl text-dimwhite">
           Projects
         </h3>
 
-        <div className="space-y-5 mt-12 grid grid-cols-1 md:grid-cols-2 md:space-y-0 md:gap-5 z-[-10] md:z-auto">
-          {pro.map((p) => (
-            <div
-              key={p}
-              className="relative flex flex-col group cursor-pointer rounded-xl overflow-hidden"
-            >
-              <img
-                src="/sd-profile.jpg"
-                alt="Project Image"
-                className="h-96 w-96 object-fill"
-              />
+        <div className="space-y-5 mt-12 grid grid-cols-1 md:grid-cols-2 md:space-y-0 md:gap-5">
+          {projects.map((project) => (
+            <Link href={`/project/${project.slug}`} key={project._id}>
+              <div className="relative flex flex-col group cursor-pointer rounded-xl overflow-hidden border border-gray-500">
+                <img
+                  src={urlFor(project.poster).url()}
+                  alt="Project Image"
+                  className="h-96 aspect-video object-contain"
+                />
 
-              <div className="absolute bottom-0 bg-black w-full p-3 space-y-2 opacity-0  group-hover:opacity-80 transition-all ease-in-out duration-300 translate-y-10 group-hover:translate-y-0">
-                <p className="font-bold">Project Name</p>
-                <p className="text-xs">Tech Stack used:</p>
+                <div className="absolute bottom-0 bg-black w-full p-3 space-y-2 opacity-0  group-hover:opacity-80 transition-all ease-in-out duration-300 translate-y-10 group-hover:translate-y-0">
+                  <p className="font-bold">{project.title}</p>
+                  <p className="text-xs">Tech Stack used:</p>
 
-                <div className="flex flex-row gap-x-4">
-                  <img src="/react.png" alt="" className="h-6 w-6" />
-                  <img src="/react.png" alt="" className="h-6 w-6" />
-                  <img src="/react.png" alt="" className="h-6 w-6" />
-                  <img src="/react.png" alt="" className="h-6 w-6" />
-                  <img src="/react.png" alt="" className="h-6 w-6" />
+                  <div className="flex flex-row gap-x-4">
+                    {project.technologies.map((technology) => (
+                      <img
+                        key={technology._id}
+                        src={urlFor(technology.image).url()}
+                        alt="technology-img"
+                        className="h-6 w-6 object-contain"
+                      />
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
-      </motion.main>
+      </main>
 
       <div className="mt-12 mb-36 group max-w-6xl mx-auto px-10 lg:pl-36">
         <Link href="/contact" className="flex gap-x-3">
