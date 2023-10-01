2ae36b747d91c8f4bc6ee6a80f3cf96fe0bb111e
Snehdeep <duparesnehdeep@gmail.com>
2023-10-02 01:29:27 +0530

    minor changes

--- app/(user)/contact/page.tsx
+++ app/(user)/contact/page.tsx
@@ -13,10 +13,10 @@ function Contact() {
       transition={{ duration: 1.2 }}
       className="md:relative flex flex-col md:flex-row text-center mx-auto max-w-6xl px-10 justify-evenly items-center h-[80vh]"
     >
-      <h3 className="tracking-[20px] absolute top-24 md:top-0 uppercase text-2xl z-[-10] md:z-auto text-dimwhite">
+      <h3 className="tracking-[20px] absolute top-24 md:top-0 uppercase text-2xl text-dimwhite">
         Contact
       </h3>
-      <h5 className="absolute top-40 md:top-12 text-dimwhite z-[-10] md:z-auto">
+      <h5 className="absolute top-40 md:top-12 text-dimwhite">
         Get in touch or shoot me an email directly on{" "}
         <span
           className="font-bold cursor-pointer hover:underline"
@@ -34,7 +34,7 @@ function Contact() {
           placeholder="Message"
           className="contact-field scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-transparent hover:scrollbar-thumb-gray-300 scrollbar-thumb-rounded-full h-28 md:h-36"
         />
-        <button className="bg-white hover:bg-dimwhite text-black py-3 px-10 rounded-md font-bold">
+        <button className="bg-white hover:bg-dimwhite text-black py-3 px-10 rounded-md font-bold transition-all duration-300 ease-in-out">
           Send Message
         </button>
       </form>
