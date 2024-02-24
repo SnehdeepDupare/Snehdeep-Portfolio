7201ab78ee87cb0a80d2c5b2a25a7268fd2de90a
Snehdeep <duparesnehdeep@gmail.com>
2024-02-24 15:57:58 +0530

    Fix Gtag script issue

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -6,8 +6,9 @@ export default function GoogleAnalytics() {
       <Script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TAG_MANAGER_ID}`}
+        strategy="afterInteractive"
       />
-      <Script id="google-analytics">
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
             window.dataLayer = window.dataLayer || []; 
             function gtag(){window.dataLayer.push(arguments);}
