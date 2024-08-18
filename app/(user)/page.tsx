934e92d073968624e9b20084c08f3959660ca03e
Snehdeep <duparesnehdeep@gmail.com>
2024-08-18 20:26:04 +0530

    Add onLoad and hover effect on Name

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -2,8 +2,78 @@
 import { Github, Linkedin, TopRightArrow } from "@/components/Icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  useEffect(() => {
+    const nameElement = document.getElementById("name");
+    let interval: NodeJS.Timeout | null = null;
+    let timeout: NodeJS.Timeout | null = null;
+
+    const scrambleText = (target: HTMLElement) => {
+      let iteration = 0;
+
+      clearInterval(interval as NodeJS.Timeout);
+
+      interval = setInterval(() => {
+        if (!target.dataset.value) return;
+
+        target.innerText = target.innerText
+          .split("")
+          .map((letter, index) => {
+            if (index < iteration) {
+              return target.dataset.value![index];
+            }
+            return letters[Math.floor(Math.random() * 26)];
+          })
+          .join("");
+
+        if (iteration >= target.dataset.value.length) {
+          clearInterval(interval as NodeJS.Timeout);
+        }
+
+        iteration += 1 / 3;
+      }, 30);
+    };
+
+    const applyEffect = () => {
+      if (window.matchMedia("(min-width: 768px)").matches) {
+        // For non-mobile devices
+        if (nameElement) {
+          nameElement.addEventListener("mouseover", (event) => {
+            const target = event.target as HTMLElement;
+            scrambleText(target);
+          });
+        }
+      }
+      // Run the effect after 600ms on page load for all devices
+      if (nameElement) {
+        timeout = setTimeout(() => {
+          scrambleText(nameElement);
+        }, 600);
+      }
+    };
+
+    applyEffect();
+
+    return () => {
+      if (nameElement && window.matchMedia("(min-width: 768px)").matches) {
+        nameElement.removeEventListener("mouseover", (event) => {
+          const target = event.target as HTMLElement;
+          scrambleText(target);
+        });
+      }
+      if (interval) {
+        clearInterval(interval);
+      }
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, [letters]);
+
   return (
     <main className="flex flex-col mt-14 md:mt-24 max-w-6xl mx-auto px-10 md:px-24">
       <motion.h3
@@ -13,7 +83,10 @@ export default function Home() {
         viewport={{ once: true }}
         className="font-semibold uppercase tracking-widest text-4xl md:text-5xl mb-5 md:mb-10"
       >
-        I am Snehdeep Dupare
+        I am{" "}
+        <span id="name" data-value="Snehdeep Dupare">
+          Snehdeep Dupare
+        </span>
       </motion.h3>
 
       <section className="space-y-5 text-sm tracking-wide leading-8 md:leading-9">
