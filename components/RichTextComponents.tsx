7b980b0d6032523cda3113648bbc1e496ea15b3c
Snehdeep <duparesnehdeep@gmail.com>
2024-08-18 20:57:56 +0530

    decrease padding

--- components/RichTextComponents.tsx
+++ components/RichTextComponents.tsx
@@ -12,22 +12,22 @@ export const RichTextComponents = {
 
   block: {
     h1: ({ children }: any) => (
-      <h1 className="text-5xl py-10 font-bold">{children}</h1>
+      <h1 className="text-5xl py-5 font-bold">{children}</h1>
     ),
     h2: ({ children }: any) => (
-      <h2 className="text-4xl py-10 font-bold">{children}</h2>
+      <h2 className="text-4xl py-5 font-bold">{children}</h2>
     ),
     h3: ({ children }: any) => (
-      <h3 className="text-3xl py-10 font-bold">{children}</h3>
+      <h3 className="text-3xl py-5 font-bold">{children}</h3>
     ),
     h4: ({ children }: any) => (
-      <h4 className="text-2xl py-10 font-bold">{children}</h4>
+      <h4 className="text-2xl py-5 font-bold">{children}</h4>
     ),
     h5: ({ children }: any) => (
-      <h5 className="text-xl py-10 font-bold">{children}</h5>
+      <h5 className="text-xl py-5 font-bold">{children}</h5>
     ),
     h6: ({ children }: any) => (
-      <h6 className="text-lg py-10 font-bold">{children}</h6>
+      <h6 className="text-lg py-5 font-bold">{children}</h6>
     ),
     normal: ({ children }: any) => <p className="py-1">{children}</p>,
   },
